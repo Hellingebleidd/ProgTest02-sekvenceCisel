3bee7454545c9ec2ee78bca23ecde15de2da5867
Fakiros <losfakiros@gmail.com>
2018-11-11 10:39:45 +0100

    Pouzite scanf miesto komplikovanej kontroly retazca. To preto, lebo strednik znamena bodkociarka a nie medzera. Vymenene poradie do while, aby sa spracovala aj 0

--- main.c
+++ main.c
@@ -84,35 +84,42 @@ int spracujVstupGetChar(long *hi, long *lo, char *operacia) {
 }
 
 
-
 int main() {
-    long hi, lo, cislo,j;
+    long hi, lo, cislo, j;
     char operacia;
-    int cifra=0,  binC[10], nula=0, s=0, sekv=0 ;
+    int cifra = 0, binC[40], nula = 0, s = 0, sekv = 0;
+    int vysledok;
 
 
-    //ak je spracovanie neuspesne (-1), skonci s chybou
+/*    //ak je spracovanie neuspesne (-1), skonci s chybou
     if (spracujVstupGetChar(&hi, &lo, &operacia) == -1)
-        return vypisNespravnyVstup();
+        return vypisNespravnyVstup();*/
+
+    printf("Zadejte interval:\n");
+    vysledok = scanf(" < %ld ; %ld > %c", &lo, &hi, &operacia);
+    if (vysledok != 3 || (operacia != 'l' && operacia != 'z' && operacia != 's') || lo > hi || lo < 0 || hi < 0) {
+        printf("Nespravny vstup.\n");
+        return -1;
+    }
 
 
     for (j = hi; j >= lo; j--) {
-        cislo=j;
-        while (cislo>0){            //ako napisem 0 alebo 9 cyhodi nespravny vstup
-            binC[j]=cislo %2;
-            cislo=cislo/2;
+        cislo = j;
+        do {            //ako napisem 0 alebo 9 cyhodi nespravny vstup
+            binC[j] = cislo % 2;
+            cislo = cislo / 2;
             cifra++;
-            if (binC[j]==0) {
+            if (binC[j] == 0) {
                 nula++;
                 s++;
-            }
-            else s=0;
-            if (sekv<s) sekv=s;
-        }
+            } else
+                s = 0;
+            if (sekv < s) sekv = s;
+        } while (cislo > 0);
     }
-    switch(operacia){
+    switch (operacia) {
         case 'l':
-            printf("Cifer: %d\n",cifra);
+            printf("Cifer: %d\n", cifra);
             break;
         case 'z':
             printf("Nul: %d\n", nula);
@@ -121,7 +128,7 @@ int main() {
             printf("Sekvence: %d\n", sekv);
             break;
         default:
-            printf ("Nespravny vstup.\n");
+            printf("Nespravny vstup.\n");
     }
 
     return 0;
